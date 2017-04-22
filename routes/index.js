fa1cf4e858d85fae1128319b0131874c3d18baf7
morganburrows <burrowmt@gmail.com>
2017-04-22 13:12:37 -0600

    Changes to landing page title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require( 'express' ),
 
 /* GET home page. */
 router.get( '/', function renderer( req, res, next ) {
-  res.render( 'index', { title: 'Express' });
+  res.render( 'index', { title: 'mrmr' });
 });
 
 module.exports = router;
