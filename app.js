70dfd4b8e8161f3a66259a8f37a0dbfbdc2ebdf9
Kyle Helmick <kyhe6996@colorado.edu>
2017-04-22 12:55:54 -0600

    Fixed scss

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use( require( 'node-sass-middleware' )({
   src: path.join( __dirname, 'public' ),
   dest: path.join( __dirname, 'public' ),
   indentedSyntax: false,
-  sourceMap: true
+  sourceMap: true,
+  debug: false
 }));
 app.use( express.static( path.join( __dirname, 'public' )));
 
