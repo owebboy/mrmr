c473498b1de502cac93804739c098efc46872beb
morganburrows <burrowmt@gmail.com>
2017-04-23 09:21:03 -0600

    added favicon, changes to favicon render, changes to layout

--- public/javascripts/UI.js
+++ public/javascripts/UI.js
@@ -1,3 +1,17 @@
+var count = 0
+title = document.title;
+var docBody = document.getElementById('site-body');
+docBody.onload = newUpdate;
+function changeTitle() {
+    count++;
+    var newTitle = '(' + count + ') ' + title;
+    document.title = newTitle;
+}
+
+function newUpdate() {
+    update = setInterval(changeTitle, 2000);
+}
+
 function sendMessage() {
   user.message = $( '#text-bar' ).val();
   if ( user.message === '' ) {
